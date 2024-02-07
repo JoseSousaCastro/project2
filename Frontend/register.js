f564b80dd89b444c902d3710e03523bd925d50e1
JoseSousaCastro <152998913+JoseSousaCastro@users.noreply.github.com>
2024-02-07 23:05:11 +0000

    placeholders do login e registo aparecem e desaparecem

--- Frontend/register.js
+++ Frontend/register.js
@@ -1,4 +1,21 @@
-function removePlaceholder(element) {
-    element.removeAttribute("placeholder");
+// Função para remover o placeholder quando há clique no input
+function removePlaceholder(input) {
+    if (input.getAttribute("data-original-placeholder")) {
+        input.placeholder = "";
+        input.addEventListener("blur", function () {
+            if (!input.value.trim()) {
+                input.placeholder = input.getAttribute("data-original-placeholder");
+            }
+        });
+    }
 }
-
+// Voltar a colocar os placeholders originais quando, após o clique, o input fica vazio 
+document.addEventListener("DOMContentLoaded", function () {
+    var inputs = document.querySelectorAll('input[placeholder]');
+    inputs.forEach(function (input) {
+        input.setAttribute("data-original-placeholder", input.getAttribute("placeholder"));
+        input.addEventListener("focus", function () {
+            removePlaceholder(input);
+        });
+    });
+});
