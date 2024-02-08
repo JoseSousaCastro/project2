77a7b809b0cbc79c8948f15a85caf66cd9587dba
Pedro Domingos <domingos@student.dei.uc.pt>
2024-02-08 02:06:04 +0000

    cirei estrutura de backend

---
+++ backend/src/main/java/aor/paj/project2/backend/dto/User.java
@@ -0,0 +1,90 @@
+package aor.paj.project2.backend.dto;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.net.URL;
+
+@XmlRootElement
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private URL photo;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email, String firstName, String lastName, int phoneNumber, URL photo) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.photo = photo;
+    }
+
+    @XmlElement
+    public String getUsername() {
+        return username;
+    }
+
+    @XmlElement
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @XmlElement
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @XmlElement
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @XmlElement
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @XmlElement
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @XmlElement
+    public URL getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(URL photo) {
+        this.photo = photo;
+    }
+}
+
+
---
+++ backend/src/main/java/aor/paj/project2/backend/service/ApplicationConfig.java
@@ -0,0 +1,7 @@
+package aor.paj.project2.backend.service;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ApplicationConfig extends Application{
+}
